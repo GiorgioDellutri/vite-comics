53018e4753dca4c0e01f9cef42af7cc708fc4387
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-10 16:25:19 +0100

    add div comicsElement with attributes

---
+++ src/components/ComicsComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'ComicsComponent',
+    props: [
+        'thumb',
+        'price',
+        'series',
+        'type',
+
+    ],
+}
+</script>
+
+<template>
+    <div class="comic-element">
+        {{ thumb }} -- {{ price }} -- {{ series }} -- {{ type }}
+    </div>
+</template>
+
+
+<style lang="scss">
+
+</style>
