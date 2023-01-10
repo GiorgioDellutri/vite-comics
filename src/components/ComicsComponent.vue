e21b9f4d920e6fb62919432486310ac59d5d539e
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-10 17:40:09 +0100

    fvarius fixed

--- src/components/ComicsComponent.vue
+++ src/components/ComicsComponent.vue
@@ -1,23 +1,46 @@
 <script>
 export default {
     name: 'ComicsComponent',
-    props: [
-        'thumb',
-        'price',
-        'series',
-        'type',
-
-    ],
+    props:
+        [
+            'thumb',
+            'price',
+            'series',
+            'type',
+        ],
+    // {
+    //     thumb: String,
+    //     price: Number,
+    //     series: String,
+    //     type: String,
+    // },
 }
 </script>
 
 <template>
     <div class="comic-element">
-        {{ thumb }} -- {{ price }} -- {{ series }} -- {{ type }}
+        <img :src="thumb" alt="thumb image">
+        <!-- {{ thumb }} -- {{ price }} -- {{ series }} -- {{ type }} -->
+        <p>{{ series }}</p>
     </div>
+    <div class="more-button">Load More</div>
 </template>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
+div.comic-element {
+    width: calc(100% / 6);
+    padding: 1rem;
 
+    img {
+        width: 100%;
+    }
+
+    p {
+        // color: $white-text-color;
+        color: white;
+        text-transform: uppercase;
+    }
+
+}
 </style>
